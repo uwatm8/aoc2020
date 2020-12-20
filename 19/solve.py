fd1c4ec207490dcf8c6f2c847580f74d360eb99a
ArvidHast <hasta@student.chalmers.se>
2020-12-20 13:24:08 +0100

    Cba

--- 19/solve.py
+++ 19/solve.py
@@ -28,29 +28,62 @@ done = False
 input = l[0]
 
 
-while not done:
+def choices(s):
+
+    c = []
+    s1 = s.split('|')[0]
+    s2 = s.split('|')[1]
+
+    c.append(s1)
+    c.append(s2)
+
+    print("choices", c)
+
+
+i = 0
+while not done and i < 4:
+    i += 1
     sps = input.split(' ')
 
-    ss = input.split('|')
-
-    if len(ss) > 1:
-        print("ss", ss)
-
     for s in sps:
-        print('s', s)
-
-        if s == '|':
-            print("split: ", )
 
         if s.isnumeric():
             inner = l[int(s)]  # .replace('|', '_', 1)
-            input = input.replace(str(s), inner, 1)
+            print("inner", inner)
+            if '|' in inner:
+                input = input.replace(str(s), '( '+inner+' )', 1)
+                # print("split: ", inner.split('|'))
+            elif len(inner.replace('" "', '').replace('"', '')) == 1:
+                input = input.replace(str(s), inner, 1)
+            else:
+                input = input.replace(str(s), '( '+inner+' )', 1)
 
-    print(input)
+    while '(' in input:
+        #print("parenthesis", input)
+        s = input.find('(')
+        e = 0
 
-    done = True
+        depth = 0
+        for i in range(len(input) - s):
+            c = input[i+s]
 
-print(ns)
-print(nn)
+            if c == '(':
+                depth += 1
+            elif c == ')':
+                depth -= 1
+
+            if depth == 0:
+                e = i+s
+                break
+
+        input = input.replace(input[s:e+1], str(solve(input[s+1:e])), 1)
+
+input = input.replace('" "', '').replace('"', '')
+print(input)
+
+choices("( ab | ba )")
+
+done = True
+
 
 print("answer: ", ans)
