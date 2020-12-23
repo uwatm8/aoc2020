8f7a249e649582a18ef20e62d7be5408f1973246
arvid_ <arvid.hast@polestar.com>
2020-12-23 01:43:11 +0100

    Update template

--- template/solve.py
+++ template/solve.py
@@ -1,19 +1,31 @@
 filepath = 'data.txt'
 
 l = []
+l2 = []
 
 with open(filepath) as fp:
     line = fp.readline()
     while line:
         l.append(line.strip())
         line = fp.readline()
-
 nls = len(l)
+
+with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+        l2.append(line.strip())
+        line = fp.readline()
+nls = len(l)
+nls2 = len(l)
 
 ans = 0
 
 for i in range(nls):
     tl = l[i]
+
+
+for i in range(nls):
+    tl = l2[i]
 
 
 print("answer: ", ans)
