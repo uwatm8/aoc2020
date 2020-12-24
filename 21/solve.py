64d06c6b8bd84d9cd58811d26885cb8b5bdba393
ArvidHast <hasta@student.chalmers.se>
2020-12-24 15:00:22 +0100

    Start 21

---
+++ 21/solve.py
@@ -0,0 +1,69 @@
+filepath = 'data1.txt'
+filepath2 = 'data2.txt'
+
+l = []
+l2 = []
+
+with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+        l.append(line.strip())
+        line = fp.readline()
+nls = len(l)
+
+with open(filepath2) as fp:
+    line = fp.readline()
+    while line:
+        l2.append(line.strip())
+        line = fp.readline()
+nls = len(l)
+nls2 = len(l2)
+
+ans = 0
+
+couldBeAlergy = {}
+couldBeIngred = {}
+allIngreds = []
+allAlergy = []
+
+for i in range(nls):
+    tl = l[i]
+
+    ingreds, alergies = tl.strip(')').split('(contains')
+
+    ingreds = tl.split(' (contains')[0].split(" ")
+    alergies = tl.split('(contains')[1].strip(')').strip(' ').split(',')
+
+    for i in ingreds:
+        if i not in allIngreds:
+            allIngreds.append(i)
+
+        if i not in couldBeAlergy:
+            couldBeAlergy[i] = []
+            for a in alergies:
+                if not a in couldBeAlergy[i]:
+                    couldBeAlergy[i].append(a)
+
+    for a in alergies:
+        if a not in allIngreds:
+            allAlergy.append(a)
+
+        if i not in couldBeAlergy:
+            couldBeAlergy[i] = []
+            for a in alergies:
+                if not a in couldBeAlergy[i]:
+                    couldBeAlergy[i].append(a)
+                couldBeIngred
+
+    print("ingreds", ingreds)
+    print("alergies", alergies)
+
+
+print(" ")
+print(couldBeAlergy)
+
+for i in range(nls2):
+    tl = l2[i]
+
+
+print("answer: ", ans)
